a1722fec58b16522d89ea08a6d662b3d09d3443d
AvinashTG <avinashavin2k04@gmail.com>
2024-07-15 11:11:35 +0530

    Matching-Tile-Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
+import GameBoard from './components/GameBoard';
+import SuccessScreen from './components/SuccessScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/game_start" element={<GameBoard />}/>
+        <Route path="/game_result" element={<SuccessScreen/>}/>
+      </Routes>
+    </Router>
   );
 }
 
